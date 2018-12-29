d6576cd66e7938c26b727f49d7a893033234880d
rohan <rohantony@cs.ajce.in>
2018-12-29 20:59:20 +0530

    this is the json file for the alexa skill intentations

---
+++ intent.js
@@ -0,0 +1,74 @@
+{
+    "interactionModel": {
+        "languageModel": {
+            "invocationName": "cookinghelp",
+            "intents": [
+                {
+                    "name": "AMAZON.CancelIntent",
+                    "samples": [
+                        "cancel"
+                    ]
+                },
+                {
+                    "name": "AMAZON.HelpIntent",
+                    "samples": [
+                        "help"
+                    ]
+                },
+                {
+                    "name": "AMAZON.StopIntent",
+                    "samples": [
+                        "stop"
+                    ]
+                },
+                {
+                    "name": "AMAZON.NavigateHomeIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.FallbackIntent",
+                    "samples": []
+                },
+                {
+                    "name": "cookinghelp",
+                    "slots": [],
+                    "samples": [
+                        "give me some cooking tips",
+                        "cooking tips",
+                        "cool cooking diy tips",
+                        "how to peel potato easily"
+                    ]
+                },
+                {
+                    "name": "AMAZON.PauseIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.ResumeIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.NextIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.PreviousIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.YesIntent",
+                    "samples": [
+                        "yes"
+                    ]
+                },
+                {
+                    "name": "AMAZON.NoIntent",
+                    "samples": [
+                        "no"
+                    ]
+                }
+            ],
+            "types": []
+        }
+    }
+}
